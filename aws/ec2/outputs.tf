e18ce6ebd98e5686733dd035b7dc7c51174ed876
Abdullah <abdullah.ajmal.7@hotmail.com>
2022-12-20 13:35:58 +0500

    Updated outputs file for ec2 module

--- aws/ec2/outputs.tf
+++ aws/ec2/outputs.tf
@@ -1,5 +1,5 @@
 output "instance_ip" {
-  value       = aws_eip.eip.public_ip
+  value       = aws_eip.eip[0].public_ip
   sensitive   = false
   description = "Publicly accessible IPv4 address of the EC2 instance."
 }
