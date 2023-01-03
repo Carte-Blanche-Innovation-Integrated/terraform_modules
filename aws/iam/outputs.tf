8e5b21588939e9cddc88e3039cd45f1f6e985e07
Abdullah <abdullah.ajmal.7@hotmail.com>
2023-01-03 10:01:05 +0500

    Updated iam module

--- aws/iam/outputs.tf
+++ aws/iam/outputs.tf
@@ -1,11 +1,11 @@
 output "iam_role_name" {
-  value       = aws_iam_role.role.id
+  value       = var.should_create ? aws_iam_role.role[0].id : null
   sensitive   = false
   description = "Name/ID of the iam role"
 }
 
 output "iam_role_arn" {
-  value       = aws_iam_role.role.arn
+  value       = var.should_create ? aws_iam_role.role[0].arn : null
   sensitive   = false
   description = "ARN of the iam role"
 }
