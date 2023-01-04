9d3840f89dc45e56b6f18456a6b1cde5319467c8
Abdullah <abdullah.ajmal.7@hotmail.com>
2023-01-04 13:34:28 +0500

    Updated code-pipeline & code-deploy modules

--- aws/code-pipeline/main.tf
+++ aws/code-pipeline/main.tf
@@ -111,8 +111,11 @@ resource "aws_iam_role_policy" "codepipeline_policy" {
     {
       "Effect": "Allow",
       "Action": [
-        "codebuild:BatchGetBuilds",
-        "codebuild:StartBuild"
+        "codedeploy:CreateDeployment",
+        "codedeploy:GetApplicationRevision",
+        "codedeploy:GetDeployment",
+        "codedeploy:GetDeploymentConfig",
+        "codedeploy:RegisterApplicationRevision"
       ],
       "Resource": "*"
     }
