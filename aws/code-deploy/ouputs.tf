e285928d16a2cb27bc678c734c284e434e2164d0
Abdullah <abdullah.ajmal.7@hotmail.com>
2023-01-04 13:18:43 +0500

    Updated code-pipeline & code-deploy modules

--- aws/code-deploy/ouputs.tf
+++ aws/code-deploy/ouputs.tf
@@ -11,7 +11,7 @@ output "app_name" {
 }
 
 output "deployment_group_name" {
-  value       = aws_codedeploy_deployment_group.group.id
+  value       = "${var.app_name}-deployment-group"
   sensitive   = false
   description = "Name of the deployment group"
 }
